92c6debeea57a571a98ba014b0d426d6125c020d
Andrea Ilic <andreailic45@gmail.com>
2023-02-05 15:23:46 +0100

    created project structure

--- Landlot.API/Program.cs
+++ Landlot.API/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,20 +8,47 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1",
+        new OpenApiInfo()
+        {
+            Title = "Landlot Service API",
+            Version = "v1.0.0",
+            Description = "Landlot.API is a microservice that provides auction lot management services. It allows you to create, update, and delete parcels, as well as generate JSON Web Tokens (JVTs) for use in securing subsequent API requests.",
+            Contact = new OpenApiContact
+            {
+                Name = "Andrea Ilić",
+                Email = "ilic.it70.2019@uns.ac.rs"
+            },
+            License = new OpenApiLicense
+            {
+                Name = "MIT licence",
+                Url = new Uri("https://opensource.org/licenses/MIT")
+            },
+            TermsOfService = new Uri("https://opensource.org/licenses/MIT")
+        });
 
-var app = builder.Build();
+    string xmlComments = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+
+    string xmlCommentsPath = Path.Combine(AppContext.BaseDirectory, xmlComments);
+
+    //  options.IncludeXmlComments(xmlCommentsPath);
+
+});
+
+WebApplication app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    _ = app.UseSwagger();
+    _ = app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
