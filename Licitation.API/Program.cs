cbe8a754fbe446330caa0422029e89a8aaa92db0
AleksandraVrekic <73581406+AleksandraVrekic@users.noreply.github.com>
2023-02-09 02:40:48 +0100

    Swagger setup

--- Licitation.API/Program.cs
+++ Licitation.API/Program.cs
@@ -1,24 +1,53 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1",
+        new OpenApiInfo()
+        {
+            Title = "Licitation Service API",
+            Version = "v1.0.0",
+            Description = "Licitation.API is a microservice developed in C# programming language that manages the process of bidding for contracts or tenders. It provides a platform for organizations to create, manage, and participate in licitations. The service generates a JSON token that serves as a secure means of identifying and accessing the information related to a specific licitation process.",
+            Contact = new OpenApiContact
+            {
+                Name = "Aleksandra Vrekić",
+                Email = "vrekic.it25.2019@uns.ac.rs"
+            },
+            License = new OpenApiLicense
+            {
+                Name = "MIT licence",
+                Url = new Uri("https://opensource.org/licenses/MIT")
+            },
+            TermsOfService = new Uri("https://opensource.org/licenses/MIT")
+        });
 
-var app = builder.Build();
+    string xmlComments = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+
+    string xmlCommentsPath = Path.Combine(AppContext.BaseDirectory, xmlComments);
+
+    //  options.IncludeXmlComments(xmlCommentsPath);
+
+});
+
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    _ = app.UseSwagger();
+    _ = app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
