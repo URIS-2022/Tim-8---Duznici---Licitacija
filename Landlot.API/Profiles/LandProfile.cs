584f8d7c9240c3ec32bcb01b44b2dc46a8912048
Andrea Ilic <andreailic45@gmail.com>
2023-02-12 00:50:06 +0100

    profiles done

---
+++ Landlot.API/Profiles/LandProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Landlot.API.Entities;
+using Landlot.API.Models;
+
+namespace Landlot.API.Profiles
+{
+    public class LandProfile : Profile
+    {
+        public LandProfile() 
+        {
+            CreateMap<Land, LandModel>();
+            CreateMap<LandCreationModel, Land>();
+            CreateMap<LandUpdateModel, Land>();
+
+        }
+    }
+}
---
+++ Landlot.API/Profiles/LotProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Landlot.API.Entities;
+using Landlot.API.Models;
+
+namespace Landlot.API.Profiles
+{
+    public class LotProfile : Profile
+    {
+        public LotProfile() 
+        { 
+            CreateMap<Lot, LotModel>();
+            CreateMap<LotCreationModel, Lot>();
+            CreateMap<LotUpdateModel, Lot>();
+        
+        }
+    }
+}
