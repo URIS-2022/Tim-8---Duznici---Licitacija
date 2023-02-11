1d363c048d9443d1c1b191a9960e49f283a23bfb
Andrea Ilic <andreailic45@gmail.com>
2023-02-12 00:43:33 +0100

    Entities done

---
+++ Landlot.API/Entities/Land.cs
@@ -0,0 +1,130 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Landlot.API.Entities
+{
+    public class Land : IValidatableObject
+    {
+        public Guid LandId { get; set; }
+
+        public int TotalArea { get; set; }
+
+        public string MunicipalityId { get; set; }
+
+        public string NameOfTheMunicipality { get; set; }
+
+        public string RealEstateNumber { get; set; }
+
+        public string LandCulture { get; set; }
+
+        public string LandClass { get; set; }
+
+        public string LandProcessing { get; set; }
+
+        public string ProtectedZone { get; set; }
+
+        public string PropertyType { get; set; }
+
+        public Guid Drainage { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+
+            // Validate TotalArea
+            if (TotalArea <= 0)
+            {
+                results.Add(new ValidationResult("TotalArea must be greater than 0"));
+            }
+
+            // Validate MunicipalityId
+            if (string.IsNullOrWhiteSpace(MunicipalityId))
+            {
+                results.Add(new ValidationResult("MunicipalityId is required"));
+            }
+            else if (MunicipalityId.Length > 50)
+            {
+                results.Add(new ValidationResult("MunicipalityId cannot be longer than 50 characters"));
+            }
+
+            // Validate NameOfTheMunicipality
+            if (string.IsNullOrWhiteSpace(NameOfTheMunicipality))
+            {
+                results.Add(new ValidationResult("NameOfTheMunicipality is required"));
+            }
+            else if (NameOfTheMunicipality.Length > 50)
+            {
+                results.Add(new ValidationResult("NameOfTheMunicipality cannot be longer than 50 characters"));
+            }
+
+            // Validate RealEstateNumber
+            if (string.IsNullOrWhiteSpace(RealEstateNumber))
+            {
+                results.Add(new ValidationResult("RealEstateNumber is required"));
+            }
+            else if (RealEstateNumber.Length > 50)
+            {
+                results.Add(new ValidationResult("RealEstateNumber cannot be longer than 50 characters"));
+            }
+
+            // Validate LandCulture
+            if (string.IsNullOrWhiteSpace(LandCulture))
+            {
+                results.Add(new ValidationResult("LandCulture is required"));
+            }
+            else if (LandCulture.Length > 50)
+            {
+                results.Add(new ValidationResult("LandCulture cannot be longer than 50 characters"));
+            }
+
+            // Validate LandClass
+            if (string.IsNullOrWhiteSpace(LandClass))
+            {
+                results.Add(new ValidationResult("LandClass is required"));
+            }
+            else if (LandClass.Length > 50)
+            {
+                results.Add(new ValidationResult("LandClass cannot be longer than 50 characters"));
+            }
+
+            // Validate LandProcessing
+            if (string.IsNullOrWhiteSpace(LandProcessing))
+            {
+                results.Add(new ValidationResult("LandProcessing is required"));
+            }
+            else if (LandProcessing.Length > 50)
+            {
+                results.Add(new ValidationResult("LandProcessing cannot be longer than 50 characters"));
+            }
+
+            // Validate ProtectedZone
+            if (string.IsNullOrWhiteSpace(ProtectedZone))
+            {
+                results.Add(new ValidationResult("ProtectedZone is required"));
+            }
+            else if (ProtectedZone.Length > 50)
+            {
+                results.Add(new ValidationResult("ProtectedZone cannot be longer than 50 characters"));
+            }
+
+            // Validate PropertyType
+            if (string.IsNullOrWhiteSpace(PropertyType))
+            {
+                results.Add(new ValidationResult("PropertyType is required"));
+            }
+            else if (PropertyType.Length > 50)
+            {
+                results.Add(new ValidationResult("PropertyType cannot be longer than 50 characters"));
+            }
+
+            // Validate Drainage
+            if (Drainage == Guid.Empty)
+            {
+                results.Add(new ValidationResult("Drainage is required"));
+            }
+
+            return results;
+        }
+
+
+    }
+}
---
+++ Landlot.API/Entities/Lot.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Landlot.API.Entities
+{
+    public class Lot : IValidatableObject
+    {
+
+        public Guid LotId { get; set; }
+
+        public DateAndTime ExpiryDate { get; set; }
+
+        public int LotArea { get; set; }
+
+        public int LotUser { get; set; }
+
+        public int LotNumber { get; set; }
+
+        public string LandCultureState { get; set; }
+
+        public string LandProcessingState { get; set; }
+
+        public string ProtectedZoneState { get; set; }
+
+        public string DrainageState { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+                var results = new List<ValidationResult>();
+
+                // Validate ExpiryDate
+                if (ExpiryDate == null)
+                {
+                    results.Add(new ValidationResult("ExpiryDate is required"));
+                }
+
+                // Validate LotArea
+                if (LotArea <= 0)
+                {
+                    results.Add(new ValidationResult("LotArea must be greater than 0"));
+                }
+
+                // Validate LotUser
+                if (LotUser <= 0)
+                {
+                    results.Add(new ValidationResult("LotUser must be greater than 0"));
+                }
+
+                // Validate LotNumber
+                if (LotNumber <= 0)
+                {
+                    results.Add(new ValidationResult("LotNumber must be greater than 0"));
+                }
+
+                // Validate LandCultureState
+                if (string.IsNullOrWhiteSpace(LandCultureState))
+                {
+                    results.Add(new ValidationResult("LandCultureState is required"));
+                }
+                else if (LandCultureState.Length > 50)
+                {
+                    results.Add(new ValidationResult("LandCultureState cannot be longer than 50 characters"));
+                }
+
+                // Validate LandProcessingState
+                if (string.IsNullOrWhiteSpace(LandProcessingState))
+                {
+                    results.Add(new ValidationResult("LandProcessingState is required"));
+                }
+                else if (LandProcessingState.Length > 50)
+                {
+                    results.Add(new ValidationResult("LandProcessingState cannot be longer than 50 characters"));
+                }
+
+                // Validate ProtectedZoneState
+                if (string.IsNullOrWhiteSpace(ProtectedZoneState))
+                {
+                    results.Add(new ValidationResult("ProtectedZoneState is required"));
+                }
+                else if (ProtectedZoneState.Length > 50)
+                {
+                    results.Add(new ValidationResult("ProtectedZoneState cannot be longer than 50 characters"));
+                }
+
+                // Validate DrainageState
+                if (string.IsNullOrWhiteSpace(DrainageState))
+                {
+                    results.Add(new ValidationResult("DrainageState is required"));
+                }
+                else if (DrainageState.Length > 50)
+                {
+                    results.Add(new ValidationResult("DrainageState cannot be longer than 50 characters"));
+                }
+
+                return results;
+            }
+
+        }
+    }
+
