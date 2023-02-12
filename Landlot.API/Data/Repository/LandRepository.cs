8b9f962225ffe72e7ef16ecb79fab39762d8bf43
Andrea Ilic <andreailic45@gmail.com>
2023-02-12 02:23:52 +0100

    repos done

--- Landlot.API/Data/Repository/LandRepository.cs
+++ Landlot.API/Data/Repository/LandRepository.cs
@@ -1,6 +1,57 @@
-﻿namespace Landlot.API.Data.Repository
+﻿using Landlot.API.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using Landlot.API.Entities;
+
+namespace Landlot.API.Data.Repository
 {
-    public class LandRepository
+    public class LandRepository : ILandRepository
     {
+        private readonly LandlotDbContext context;
+
+        public LandRepository(LandlotDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Land>> GetLands()
+        {
+            return await context.Lands.ToListAsync();
+        }
+
+        public async Task<Land?> GetLand(Guid id)
+        {
+            return await context.Lands.FindAsync(id);
+        }
+
+        public async Task<Land?> UpdateLand(Guid id,Land updateModel)
+        {
+            var land = await context.Lands.FirstOrDefaultAsync(c => c.LandGuid == id);
+            if (land == null)
+            {
+                return null;
+            }
+            context.Entry(land).CurrentValues.SetValues(updateModel);
+            await context.SaveChangesAsync();
+            return land;
+        }
+
+        public async Task<Land?> AddLand(Land land)
+        {
+            var created = context.Lands.Add(land);
+            await context.SaveChangesAsync();
+            return created.Entity;
+        }
+
+        public async Task DeleteLand(Guid id)
+        {
+            var systemUser = await context.Lands.FindAsync(id);
+            if (systemUser == null)
+            {
+                throw new InvalidOperationException("Land not found");
+            }
+            context.Lands.Remove(systemUser);
+            await context.SaveChangesAsync();
+        }
+
     }
 }
--- Landlot.API/Data/Repository/LotRepository.cs
+++ Landlot.API/Data/Repository/LotRepository.cs
@@ -1,6 +1,55 @@
-﻿namespace Landlot.API.Data.Repository
+﻿using Landlot.API.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using Landlot.API.Entities;
+
+namespace Landlot.API.Data.Repository
 {
-    public class LotRepository
+    public class LotRepository : ILotRepository
     {
+        private readonly LandlotDbContext context;
+
+        public LotRepository(LandlotDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IEnumerable<Lot>> GetLots()
+        {
+            return await context.Lots.ToListAsync();
+        }
+
+        public async Task<Lot?> GetLot(Guid id)
+        {
+            return await context.Lots.FindAsync(id);
+        }
+
+        public async Task<Lot?> UpdateLot(Guid id, Lot updateModel)
+        {
+            var lot = await context.Lots.FirstOrDefaultAsync(c => c.LotGuid == id);
+            if (lot == null)
+            {
+                return null;
+            }
+            context.Entry(lot).CurrentValues.SetValues(updateModel);
+            await context.SaveChangesAsync();
+            return lot;
+        }
+
+        public async Task<Lot?> AddLot(Lot lot)
+        {
+            var created = context.Lots.Add(lot);
+            await context.SaveChangesAsync();
+            return created.Entity;
+        }
+
+        public async Task DeleteLot(Guid id)
+        {
+            var systemUser = await context.Lots.FindAsync(id);
+            if (systemUser == null)
+            {
+                throw new InvalidOperationException("Lot not found");
+            }
+            context.Lots.Remove(systemUser);
+            await context.SaveChangesAsync();
+        }
     }
 }
