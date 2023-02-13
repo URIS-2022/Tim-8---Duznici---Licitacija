331e0cb02df3f5532a6331534b832c966710b12d
Dejan Petrov <73533915+DejanDeja@users.noreply.github.com>
2023-02-13 22:59:32 +0100

    update program cs

--- Preparation.API/Program.cs
+++ Preparation.API/Program.cs
@@ -1,15 +1,81 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Preparation.API.Data;
+using Preparation.API.Data.Repository;
+using Preparation.API.Enums;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
-builder.Services.AddDbContext<PreparationDBContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers(setup =>
+            setup.ReturnHttpNotAcceptable = true
+        ).AddXmlDataContractSerializerFormatters() // Dodajemo podršku za XML tako da ukoliko klijent to traži u Accept header-u zahteva možemo da serializujemo payload u XML u odgovoru.
+        .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.Converters.Add(new DocumentTypeConverter());
+            options.JsonSerializerOptions.Converters.Add(new DocumentStatusConverter());
+        }
+        )
+        .ConfigureApiBehaviorOptions(setupAction => // Deo koji se odnosi na podržavanje Problem Details for HTTP APIs
+        {
+            setupAction.InvalidModelStateResponseFactory = context =>
+            {
+                // Kreiramo problem details objekat
+                ProblemDetailsFactory problemDetailsFactory = context.HttpContext.RequestServices
+                    .GetRequiredService<ProblemDetailsFactory>();
 
-builder.Services.AddControllers();
+                // Prosle?ujemo trenutni kontekst i ModelState, ovo prevodi validacione greške iz ModelState-a u RFC format
+                ValidationProblemDetails problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                    context.HttpContext,
+                    context.ModelState);
+
+                // Ubacujemo dodatne podatke
+                problemDetails.Detail = "See errors fields for more info.";
+                problemDetails.Instance = context.HttpContext.Request.Path;
+
+                // Podrazumevano se sve vra?a kao status 400 BadRequest, to je ok kada nisu u pitanju validacione greške,
+                // ako jesu ho?emo da koristimo status 422 UnprocessibleEntity
+                // tražimo info koji status kod da koristimo
+                ActionExecutingContext? actionExecutiongContext = context as ActionExecutingContext;
+
+                // Proveravamo da li postoji neka greška u ModelState-u, a tako?e proveravamo da li su svi prosle?eni parametri dobro parsirani
+                // ako je sve ok parsirano ali postoje greške u validaciji ho?emo da vratimo status 422
+                if ((context.ModelState.ErrorCount > 0) &&
+                    (actionExecutiongContext?.ActionArguments.Count == context.ActionDescriptor.Parameters.Count))
+                {
+                    problemDetails.Type = "https://google.com"; // ina?e treba da stoji link ka stranici sa detaljima greške
+                    problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                    problemDetails.Title = "A validation error occurred.";
+
+                    // Sve vra?amo kao UnprocessibleEntity objekat
+                    return new UnprocessableEntityObjectResult(problemDetails)
+                    {
+                        ContentTypes = { "application/problem+json" }
+                    };
+                }
+
+                // Ukoliko postoji nešto što nije moglo da se parsira ho?emo da vra?amo status 400 kao i do sada
+                problemDetails.Status = StatusCodes.Status400BadRequest;
+                problemDetails.Title = "An error occurred while parsing the submitted content.";
+                return new BadRequestObjectResult(problemDetails)
+                {
+                    ContentTypes = { "application/problem+json" }
+                };
+            };
+        });
+
+builder.Services.AddDbContext<PreparationDBContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'PreparationDBContext' not found.")));
+builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+//builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 //builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
@@ -51,15 +117,18 @@ builder.Services.AddSwaggerGen(options =>
     options.IncludeXmlComments(xmlCommentsPath);
 });
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 app.UseCors("CorsPolicy");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    _ = app.UseSwagger();
+    _ = app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
 
 app.UseRouting();
