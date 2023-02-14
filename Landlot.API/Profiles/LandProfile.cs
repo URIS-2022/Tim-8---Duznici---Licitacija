fe8b82874eb656ad04f84c5c8b6db703f8f97b47
Andrea Ilic <andreailic45@gmail.com>
2023-02-14 09:36:00 +0100

    profils updated

--- Landlot.API/Profiles/LandProfile.cs
+++ Landlot.API/Profiles/LandProfile.cs
@@ -14,7 +14,14 @@ namespace Landlot.API.Profiles
             CreateMap<LandPostRequestModel, Land>();
             CreateMap<LandPatchRequestModel, Land>()
             .ForMember(dest => dest.TotalArea, opt => opt.Condition(src => src.TotalArea != null))
-            .ForMember(dest => dest.RealEstateNumber, opt => opt.Condition(src => src.RealEstateNumber != null));
+            .ForMember(dest => dest.RealEstateNumber, opt => opt.Condition(src => src.RealEstateNumber != null))
+            .ForMember(dest => dest.Municipality, opt => opt.MapFrom(src => src.Municipality!.Value))
+            .ForMember(dest => dest.Culture, opt => opt.MapFrom(src => src.Culture!.Value))
+            .ForMember(dest => dest.LandClass, opt => opt.MapFrom(src => src.LandClass!.Value))
+            .ForMember(dest => dest.Processing, opt => opt.MapFrom(src => src.Processing!.Value))
+            .ForMember(dest => dest.Zone, opt => opt.MapFrom(src => src.Zone!.Value))
+            .ForMember(dest => dest.Property, opt => opt.MapFrom(src => src.Property!.Value))
+            .ForMember(dest => dest.Drainage, opt => opt.MapFrom(src => src.Drainage!.Value));
         }
     }
 }
--- Landlot.API/Profiles/LotProfile.cs
+++ Landlot.API/Profiles/LotProfile.cs
@@ -15,7 +15,12 @@ namespace Landlot.API.Profiles
             CreateMap<LotPatchRequestModel, Lot>()
             .ForMember(dest => dest.LotUser, opt => opt.Condition(src => src.LotUser != null))
             .ForMember(dest => dest.LotNumber, opt => opt.Condition(src => src.LotNumber != null))
-            .ForMember(dest => dest.LotArea, opt => opt.Condition(src => src.LotArea != null));
+            .ForMember(dest => dest.LotArea, opt => opt.Condition(src => src.LotArea != null))
+            .ForMember(dest => dest.CultureState, opt => opt.MapFrom(src => src.CultureState!.Value))
+            .ForMember(dest => dest.ClassState, opt => opt.MapFrom(src => src.ClassState!.Value))
+            .ForMember(dest => dest.ProcessingState, opt => opt.MapFrom(src => src.ProcessingState!.Value))
+            .ForMember(dest => dest.ProtectedZoneState, opt => opt.MapFrom(src => src.ProtectedZoneState!.Value))
+            .ForMember(dest => dest.DrainageState, opt => opt.MapFrom(src => src.DrainageState!.Value));
 
 
         }
