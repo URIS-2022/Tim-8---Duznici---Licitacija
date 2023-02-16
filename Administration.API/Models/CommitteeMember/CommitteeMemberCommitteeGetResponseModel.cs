9e3a00303f320b721490c4ad570fb7e4ae3c3d8d
Sandra Stojanov <sandra.stojanov999@gmail.com>
2023-02-16 22:40:31 +0100

    Finish XML comments for Administration

--- Administration.API/Models/CommitteeMember/CommitteeMemberCommitteeGetResponseModel.cs
+++ Administration.API/Models/CommitteeMember/CommitteeMemberCommitteeGetResponseModel.cs
@@ -2,16 +2,34 @@
 
 namespace Administration.API.Models.CommitteeMember;
 
+/// <summary>
+/// Represents a CommitteeMember in CommitteeGetResponseModel.
+/// </summary>
 [DataContract(Name = "CommitteeMember", Namespace = "")]
 public class CommitteeMemberCommitteeGetResponseModel
 {
+    /// <summary>
+    /// Gets or sets the unique identifier of the committee.
+    /// </summary>
     [DataMember]
     public Guid Guid { get; set; }
+
+    /// <summary>
+    /// Gets or sets the first name of the member.
+    /// </summary>
     [DataMember]
-    public string FirstName { get; set; }
+    public string? FirstName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the last name of the member.
+    /// </summary>
     [DataMember]
-    public string LastName { get; set; }
+    public string? LastName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the rome of the member in committee.
+    /// </summary>
     [DataMember]
-    public string Role { get; set; }
+    public string? Role { get; set; }
 
 }
