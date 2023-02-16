3cce7864b87301dbbec471101941bd52523db277
Andrea Ilic <andreailic45@gmail.com>
2023-02-16 19:07:27 +0100

    Revert "smells in repositories"

--- Person.API/Data/Repository/ILegalPersonRepository.cs
+++ Person.API/Data/Repository/ILegalPersonRepository.cs
@@ -6,43 +6,12 @@ using System.Threading.Tasks;
 
 namespace Person.API.Data.Repository
 {
-    /// <summary>
-    /// Interface for managing legal persons in the database.
-    /// </summary>
     public interface ILegalPersonRepository
     {
-        /// <summary>
-        /// Gets all legal persons from the database.
-        /// </summary>
-        /// <returns>An IEnumerable of LegalPerson.</returns>
         Task<IEnumerable<LegalPerson>> GetAllLegalPersons();
-
-        /// <summary>
-        /// Gets an legal person by its unique identifier.
-        /// </summary>
-        /// <param name="LegalPersonId">The unique identifier of the legal person.</param>
-        /// <returns>An LegalPerson object if found; otherwise, null.</returns>
         Task<LegalPerson?> GetLegalPersonByGuid(Guid LegalPersonId);
-
-        /// <summary>
-        /// Creates a new legal person in the database.
-        /// </summary>
-        /// <param name="legalPerson">The legal person to be created.</param>
-        /// <returns>The newly created LegalPerson object.</returns>
         Task<LegalPerson?> CreateLegalPerson(LegalPerson? legalPerson);
-
-        /// <summary>
-        /// Deletes an legal person from the database.
-        /// </summary>
-        /// <param name="LegalPersonId">The unique identifier of the legal person to be deleted.</param>
         Task DeleteLegalPerson(Guid LegalPersonId);
-
-        /// <summary>
-        /// Updates an existing legal person in the database.
-        /// </summary>
-        /// <param name="id">The unique identifier of the legal person to be updated.</param>
-        /// <param name="legalPerson">The updated legal person data.</param>
-        /// <returns>The updated LegalPerson object if successful; otherwise, null.</returns>
         Task<LegalPerson?> UpdateLegalPerson(Guid id, LegalPerson legalPerson);
 
     }
--- Person.API/Data/Repository/LegalPersonRepository.cs
+++ Person.API/Data/Repository/LegalPersonRepository.cs
@@ -12,41 +12,28 @@ using System.Text.Json;
 namespace Person.API.Data.Repository
 
 {
-    /// <summary>
-    /// Provides CRUD operations for the LegalPerson entity.
-    /// </summary>
     public class LegalPersonRepository : ILegalPersonRepository
     { 
         private readonly PersonDbContext context;
 
-        /// <summary>
-        /// Creates a new instance of the LegalPersonRepository class.
-        /// </summary>
-        /// <param name="context">The PersonDbContext object.</param>
 
         public LegalPersonRepository(PersonDbContext context)
         {
             this.context = context;
 
         }
-
-        /// <summary>
-        /// Retrieves all legal persons from the database.
-        /// </summary>
-        /// <returns>An IEnumerable collection of LegalPerson objects.</returns>
         public async Task<IEnumerable<LegalPerson>> GetAllLegalPersons()
         {
+            var options = new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve
+            };
 
             return await context.LegalPersons
                .Include(c => c.ContactPerson)
                 .ToListAsync();
         }
 
-        /// <summary>
-        /// Retrieves an legal person from the database by its ID.
-        /// </summary>
-        /// <param name="LegalPersonId">The ID of the legal person to retrieve.</param>
-        /// <returns>The LegalPerson object with the specified ID, or null if no such legal person exists.</returns>
         public async Task<LegalPerson?> GetLegalPersonByGuid(Guid LegalPersonId)
         {
 
@@ -56,23 +43,13 @@ namespace Person.API.Data.Repository
             
         }
 
-        /// <summary>
-        /// Adds a new legal person to the database.
-        /// </summary>
-        /// <param name="legalPerson">The LegalPerson object to be added.</param>
-        /// <returns>The LegalPerson object that was added to the database.</returns>
-        public async Task<LegalPerson?> CreateLegalPerson(LegalPerson? legalPerson)
+        public async Task<LegalPerson> CreateLegalPerson(LegalPerson legalPerson)
         {
             context.LegalPersons.Add(legalPerson);
             await context.SaveChangesAsync();
             return legalPerson;
         }
 
-        /// <summary>
-        /// Deletes an legal person from the database by its ID.
-        /// </summary>
-        /// <param name="LegalPersonId">The ID of the legal person to delete.</param>
-        /// <returns>A Task representing the asynchronous operation.</returns>
         public async Task DeleteLegalPerson(Guid LegalPersonId)
         {
             var legalPerson = await GetLegalPersonByGuid(LegalPersonId);
@@ -80,12 +57,6 @@ namespace Person.API.Data.Repository
             await context.SaveChangesAsync();
         }
 
-        /// <summary>
-        /// Updates an existing legal person in the database.
-        /// </summary>
-        /// <param name="id">The ID of the legal person to update.</param>
-        /// <param name="updateModel">The updated LegalPerson object.</param>
-        /// <returns>The LegalPerson object that was updated in the database, or null if no such legal person exists.</returns>
         public async Task<LegalPerson?> UpdateLegalPerson(Guid id, LegalPerson updateModel)
         {
             var legalPerson = await context.LegalPersons.FirstOrDefaultAsync(c => c.LegalPersonId == id);
