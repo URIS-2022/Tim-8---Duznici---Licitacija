ac990ce8a7d95ccf199be309fdaeef173fae9737
Andrea Ilic <andreailic45@gmail.com>
2023-02-16 18:31:29 +0100

    smells in repositories

--- Person.API/Data/Repository/IAddressRepository.cs
+++ Person.API/Data/Repository/IAddressRepository.cs
@@ -6,12 +6,44 @@ using System.Threading.Tasks;
 
 namespace Person.API.Data.Repository
 {
+    /// <summary>
+    /// Interface for managing addresses in the database.
+    /// </summary>
     public interface IAddressRepository
     {
+        /// <summary>
+        /// Gets all addresses from the database.
+        /// </summary>
+        /// <returns>An IEnumerable of Address.</returns>
         Task<IEnumerable<Address>> GetAllAddresses();
-        Task<Address?> GetAddressByGuid(Guid AddressId);
-        Task<Address?> CreateAddress(Address? address);
-        Task DeleteAddress(Guid AddressId);
+
+        /// <summary>
+        /// Gets an address by its unique identifier.
+        /// </summary>
+        /// <param name="addressId">The unique identifier of the address.</param>
+        /// <returns>An Address object if found; otherwise, null.</returns>
+        Task<Address?> GetAddressByGuid(Guid addressId);
+
+        /// <summary>
+        /// Creates a new address in the database.
+        /// </summary>
+        /// <param name="address">The address to be created.</param>
+        /// <returns>The newly created Address object.</returns>
+        Task<Address> CreateAddress(Address address);
+
+        /// <summary>
+        /// Deletes an address from the database.
+        /// </summary>
+        /// <param name="addressId">The unique identifier of the address to be deleted.</param>
+        Task DeleteAddress(Guid addressId);
+
+        /// <summary>
+        /// Updates an existing address in the database.
+        /// </summary>
+        /// <param name="id">The unique identifier of the address to be updated.</param>
+        /// <param name="address">The updated address data.</param>
+        /// <returns>The updated Address object if successful; otherwise, null.</returns>
         Task<Address?> UpdateAddress(Guid id, Address address);
     }
+
 }
--- Person.API/Data/Repository/IContactPersonRepository.cs
+++ Person.API/Data/Repository/IContactPersonRepository.cs
@@ -6,12 +6,44 @@ using System.Threading.Tasks;
 
 namespace Person.API.Data.Repository
 {
+    /// <summary>
+    /// Interface for managing contact persoons in the database.
+    /// </summary>
     public interface IContactPersonRepository
     {
+
+        /// <summary>
+        /// Gets all contact persons from the database.
+        /// </summary>
+        /// <returns>An IEnumerable of ContactPerson.</returns>
         Task<IEnumerable<ContactPerson>> GetAllContactPersons();
+
+        /// <summary>
+        /// Gets an contact person by its unique identifier.
+        /// </summary>
+        /// <param name="ContactPersonId">The unique identifier of the contact perso.</param>
+        /// <returns>An ContactPerson object if found; otherwise, null.</returns>
         Task<ContactPerson?> GetContactPersonByGuid(Guid ContactPersonId);
+
+        /// <summary>
+        /// Creates a new contact person in the database.
+        /// </summary>
+        /// <param name="contactPerson">The contact person to be created.</param>
+        /// <returns>The newly created ContactPerson object.</returns>
         Task<ContactPerson?> CreateContactPerson(ContactPerson? contactPerson);
+
+        /// <summary>
+        /// Deletes an contact person from the database.
+        /// </summary>
+        /// <param name="ContactPersonId">The unique identifier of the contact person to be deleted.</param>
         Task DeleteContactPerson(Guid ContactPersonId);
+
+        /// <summary>
+        /// Updates an existing contact person in the database.
+        /// </summary>
+        /// <param name="id">The unique identifier of the contact person to be updated.</param>
+        /// <param name="contactPerson">The updated contact person data.</param>
+        /// <returns>The updated ContactPerson object if successful; otherwise, null.</returns>
         Task<ContactPerson?> UpdateContactPerson(Guid id, ContactPerson contactPerson);
     }
 }
