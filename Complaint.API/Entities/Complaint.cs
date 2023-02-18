84d2e970ec5970cb6e3db927e52cae33e804f4be
Sandra Stojanov <sandra.stojanov999@gmail.com>
2023-02-18 15:01:35 +0100

    Fix sonar errors

--- Complaint.API/Entities/Complaint.cs
+++ Complaint.API/Entities/Complaint.cs
@@ -4,44 +4,77 @@ using System.Text.Json.Serialization;
 
 namespace Complaint.API.Entities;
 
+/// <summary>
+/// Represents a complaint
+/// </summary>
 public class Complaint : IValidatableObject
 {
+    /// <summary>
+    /// Unique identifier for the complaint
+    /// </summary>
     [Key]
     public Guid Guid { get; set; }
+
+    /// <summary>
+    /// Type of the complaint
+    /// </summary>
     [JsonConverter(typeof(ComplaintTypeConverter))]
     public ComplaintType Type { get; set; }
+
+    /// <summary>
+    /// Date the complaint was submitted
+    /// </summary>
     public DateTime DateSubmitted { get; set; }
+
+    /// <summary>
+    /// Guid of the buyer
+    /// </summary>
     public Guid BuyerGuid { get; set; }
+
+    /// <summary>
+    /// Reason for the complaint
+    /// </summary>
     public string Reason { get; set; }
+
+    /// <summary>
+    /// Rationale for the complaint
+    /// </summary>
     public string Rationale { get; set; }
+
+    /// <summary>
+    /// Date the complaint was resolved
+    /// </summary>
     public DateTime ResolutionDate { get; set; }
+
+    /// <summary>
+    /// Resolution code for the complaint
+    /// </summary>
     public string ResolutionCode { get; set; }
     [JsonConverter(typeof(ComplaintStatusConverter))]
+
+    /// <summary>
+    /// Status of the complaint
+    /// </summary>
     public ComplaintStatus Status { get; set; }
+
+    /// <summary>
+    /// Guid of the subject of the complaint
+    /// </summary>
     public Guid SubjectGuid { get; set; }
+
+    /// <summary>
+    /// Action taken for the complaint
+    /// </summary>
     [JsonConverter(typeof(ComplaintActionConverter))]
     public ComplaintAction Action { get; set; }
 
-    public Complaint(Guid guid, ComplaintType type, DateTime dateSubmitted, Guid buyerGuid, string reason, string rationale,
-    DateTime resolutionDate, string resolutionCode, ComplaintStatus status, Guid subjectGuid, ComplaintAction action)
-    {
-        Guid = guid;
-        Type = type;
-        DateSubmitted = dateSubmitted;
-        BuyerGuid = buyerGuid;
-        Reason = reason;
-        Rationale = rationale;
-        ResolutionDate = resolutionDate;
-        ResolutionCode = resolutionCode;
-        Status = status;
-        SubjectGuid = subjectGuid;
-        Action = action;
-    }
-
+    /// <summary>
+    /// Constructor for Complaint
+    /// </summary>
     public Complaint(ComplaintType type, DateTime dateSubmitted, Guid buyerGuid, string reason, string rationale,
-    DateTime resolutionDate, string resolutionCode, ComplaintStatus status, Guid subjectGuid, ComplaintAction action)
+    DateTime resolutionDate, string resolutionCode, ComplaintStatus status, Guid subjectGuid, ComplaintAction action, Guid? guid = null)
     {
-        Guid = Guid.NewGuid();
+        Guid = guid ?? Guid.NewGuid();
         Type = type;
         DateSubmitted = dateSubmitted;
         BuyerGuid = buyerGuid;
@@ -54,10 +87,14 @@ public class Complaint : IValidatableObject
         Action = action;
     }
 
-
+    /// <summary>
+    /// Validates the complaint
+    /// </summary>
+    /// <param name="validationContext"> ValidationContext for the complaint</param>
+    /// <returns> IEnumerable of ValidationResult</returns>
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
-        List<ValidationResult> results = new List<ValidationResult>();
+        List<ValidationResult> results = new();
 
         if (DateSubmitted > DateTime.Now)
         {
@@ -86,5 +123,4 @@ public class Complaint : IValidatableObject
 
         return results;
     }
-
 }
--- Complaint.API/Enums/ComplaintAction.cs
+++ Complaint.API/Enums/ComplaintAction.cs
@@ -3,13 +3,29 @@ using System.Text.Json.Serialization;
 
 namespace Complaint.API.Enums;
 
+/// <summary>
+/// Action to be taken for the complaint
+/// </summary>
 public enum ComplaintAction
 {
+    /// <summary>
+    /// No second round
+    /// </summary>
     NoSecondRound = 0,
+
+    /// <summary>
+    /// Second round with new conditions
+    /// </summary>
     SecondRoundNewConditions,
+    /// <summary>
+    /// Second round with same conditions
+    /// </summary>
     SecondRoundSameConditions
 }
 
+/// <summary>
+/// JSON converter for ComplaintAction
+/// </summary>
 public class ComplaintActionConverter : JsonConverter<ComplaintAction>
 {
     private readonly Dictionary<ComplaintAction, string> _actionMapping = new()
@@ -19,6 +35,14 @@ public class ComplaintActionConverter : JsonConverter<ComplaintAction>
         { ComplaintAction.NoSecondRound, "JN ne ide u drugi krug" },
     };
 
+    /// <summary>
+    /// Read override for ComplaintActionConverter
+    /// </summary>
+    /// <param name="reader"> Utf8JsonReader for ComplaintActionConverter</param>
+    /// <param name="typeToConvert"> Type to convert for ComplaintActionConverter</param>
+    /// <param name="options"> JsonSerializerOptions for ComplaintActionConverter</param>
+    /// <returns> ComplaintAction</returns>
+    /// <exception cref="JsonException"> Unable to map role string to complaint action</exception>
     public override ComplaintAction Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         string actionString = reader.GetString() ?? "JN ne ide u drugi krug";
@@ -33,6 +57,12 @@ public class ComplaintActionConverter : JsonConverter<ComplaintAction>
         throw new JsonException($"Unable to map role string '{actionString}' to complaint action.");
     }
 
+    /// <summary>
+    /// Write override for ComplaintActionConverter
+    /// </summary>
+    /// <param name="writer"> Utf8JsonWriter for ComplaintActionConverter</param>
+    /// <param name="value"> ComplaintAction to write</param>
+    /// <param name="options"> JsonSerializerOptions for ComplaintActionConverter</param>
     public override void Write(Utf8JsonWriter writer, ComplaintAction value, JsonSerializerOptions options)
     {
         writer.WriteStringValue(_actionMapping[value]);
